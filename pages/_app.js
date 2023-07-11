2bdfc9644804c2d7e13d39cd4ffacd9de8b55c58
kalburgimanjunath <1643278+kalburgimanjunath@users.noreply.github.com>
2023-07-11 18:56:16 +0530

    1 file modified

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,5 @@
 import '../styles/globals.css';
-import { positions, Provider } from 'react-alert';
-import AlertTemplate from 'react-alert-template-basic';
+
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
